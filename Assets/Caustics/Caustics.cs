20c6413b2e2714813e3ca0562db89494b86835d6
C. Wong <cwong4311@gmail.com>
2019-09-28 17:03:39 +1000

    Adding Features 4,8,9,10,11

---
+++ Assets/Caustics/Caustics.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class Caustics : MonoBehaviour
+{
+    private Projector projector;
+    public UnityEngine.Video.VideoClip mt;
+ 
+    void Start()
+    {
+        var vp = gameObject.AddComponent<UnityEngine.Video.VideoPlayer>();
+        vp.playOnAwake = false;
+        vp.clip = mt;
+        vp.isLooping = true;
+
+        //vp.renderMode = UnityEngine.Video.VideoRenderMode.MaterialOverride;
+        //vp.targetMaterialRenderer = GetComponent<Renderer>();
+        //vp.targetMaterialProperty = "_ShadowTex";
+
+        projector = GetComponent<Projector>();
+        projector.material.SetTexture("_ShadowTex", vp.texture);
+        
+        vp.Play();
+    }
+}
---
+++ Assets/scripts/fog.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fog : MonoBehaviour
+{
+    public GameObject plane;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RenderSettings.fogColor = Camera.main.backgroundColor;
+        RenderSettings.fogDensity = 0.1f;
+        RenderSettings.fog = true;
+    }
+}
